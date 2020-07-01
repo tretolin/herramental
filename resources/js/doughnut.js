d479d6f49e4f34a6ddfb8d9ef56ed497177f534c
Luis Treto <luisjavier.treto@gmail.com>
2020-07-01 17:08:44 -0500

    Texto en interior de gráfico

--- resources/js/doughnut.js
+++ resources/js/doughnut.js
@@ -32,6 +32,38 @@ export function doughnut () {
     }
 }
 
+// Texto interior
+Chart.pluginService.register({
+    beforeDraw: function(chart) {
+    var width = chart.chart.width,
+        height = chart.chart.height,
+        ctx = chart.chart.ctx;
+
+    ctx.restore();
+    var fontSize = (height / 184).toFixed(2);
+    ctx.font = fontSize + "em sans-serif";
+    ctx.textBaseline = "middle";
+    ctx.fillStyle = '#34569e';
+
+    var text = "$1,605.00",
+        textX = Math.round((width - ctx.measureText(text).width) / 2),
+        textY = height / 2;
+
+    ctx.fillText(text, textX, textY);
+
+    var fontSize = (height / 224).toFixed(2);
+    ctx.font = fontSize + "em sans-serif";
+
+    var text = "Total",
+    textX = Math.round((width - ctx.measureText(text).width) / 2),
+    textY = height / 1.7;
+
+    ctx.fillText(text, textX, textY);
+
+    ctx.save();
+    }
+});
+
 export default {
     doughnut
 }

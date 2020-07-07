d61f5a401a765a04a76ec0723ea48a1a9f9acd4b
Luis Treto <luisjavier.treto@gmail.com>
2020-07-07 15:08:25 -0500

    Api folder

---
+++ resources/js/Api/api.js
@@ -0,0 +1,56 @@
+import Axios from "axios";
+import { compileToFunctions } from "vue-template-compiler";
+
+
+const applicationJSON = 'application/json; charset=utf8';
+const applicationEncode = 'application/x-www-form-urlencoded';
+const baseURL = 'https://widcqzoxhb.execute-api.us-east-2.amazonaws.com/Prod/api/';
+
+const api = {
+    // login  - - - - - - - - - - - - - - - - - - - - -
+    login: function(data, callback) {
+        $.ajax({
+            method: 'POST',
+            url: baseURL + 'user/authenticate',
+            data: data,
+            beforeSend: function(xhr) {
+                xhr.setRequestHeader('Content-Type', applicationJSON);
+            },
+            xhrFields: {
+                crossorigin: true
+            },
+            headers: { 'Content-Type': 'application/json' },
+            error: function(jqXHR) {
+                callback([]);
+                return [];
+            }
+        }).done(function(data) {
+            callback(data);
+            return data;
+        });
+    },
+    // Recovery Password  - - - - - - - - - - - - - - - - - - - - -
+    passwordRecovery: function(data, callback) {
+        $.ajax({
+            method: 'POST',
+            url: baseURL + 'user/password-recovery',
+            data: data,
+            beforeSend: function(xhr) {
+                xhr.setRequestHeader('Content-Type', applicationJSON);
+            },
+            headers: { },
+            error: function(jqXHR) {
+                callback([]);
+                return [];
+            }
+        }).done(function(data) {
+            callback(data);
+            return data;
+        });
+    },
+    // Signout - - - - - - - - - - - - - - - - - - - - -
+    signOut: function(callback) {
+    },
+};
+
+export default api;
